272d7c1abf3ec614053ea552227107ebbb1b0d98
Amir <atenyun@gmail.com>
2023-12-12 03:33:59 +0330

    Add initial files

---
+++ src/main.jsx
@@ -0,0 +1,51 @@
+import React, { Suspense, lazy } from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
+import { AuthProvider } from './contexts/AuthContext'
+import './index.scss'
+import './styles/global.scss'
+
+import ErrorPage from './error-page'
+const Layout = lazy(() => import('./routes/layout.jsx'))
+import SplashScreen, { loader as splashScreenLoader } from './routes/splashScreen.jsx'
+import Home from './routes/home.jsx'
+import Loading from './routes/components/LoadingSpinner'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <Suspense fallback={<Loading />}>
+        <AuthProvider>
+          <Layout />
+        </AuthProvider>
+      </Suspense>
+    ),
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        loader: splashScreenLoader,
+        element: <SplashScreen title={`Welcome`} />,
+      },
+      {
+        path: 'home',
+        element: <Home title={`Home`} />,
+      },
+      {
+        path: 'card',
+        element: (
+          <AuthProvider>
+            <>card</>
+          </AuthProvider>
+        ),
+      },
+    ],
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // <React.StrictMode>
+  <RouterProvider router={router} />
+  // </React.StrictMode>
+)
